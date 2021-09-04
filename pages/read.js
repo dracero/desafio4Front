37673883e585415d43bf47e8f298ccd76b04227c
dracero <diego.racero@gmail.com>
2021-09-04 13:12:38 -0300

    readServerSide

--- pages/read.js
+++ pages/read.js
@@ -1,34 +1,41 @@
 import Link from "next/link";
 
 const Productos = ({ productos }) => {
-  return (
-    <div>
-      <h1>Desafio 4 Lectura general</h1>
-      <table>
-        <tr>
-          <th>Title</th>
-          <th>Price</th>
-          <th>Thumbnail</th>
-        </tr>
-        {productos.map((prod) => (
+  if (Array.isArray(productos)) {
+    return (
+      <div>
+        <h1>Desafio 4 Lectura general</h1>
+        <table>
           <tr>
-            <td>{prod.title}</td>
-            <td>{prod.price}</td>
-            <td>{prod.thumbnail}</td>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Thumbnail</th>
           </tr>
-        ))}
-      </table>
+          {productos.map((prod) => (
+            <tr>
+              <td>{prod.title}</td>
+              <td>{prod.price}</td>
+              <td>{prod.thumbnail}</td>
+            </tr>
+          ))}
+        </table>
+        <Link href="/">
+          <a>Volver al menú</a>
+        </Link>
+      </div>
+    );
+  } else
+    return (
       <Link href="/">
         <a>Volver al menú</a>
       </Link>
-    </div>
-  );
+    );
 };
 
 export default Productos;
 
 //acá leo, llamo a getStaticProps para que en producción se pueda cachear
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch("https://ds4-cdrhouse.herokuapp.com/api/productos/");
   const data = await res.json();
 
